7dae8b481bb40e823b64fb1d0e190e9d709a7ba1
Dener Fernandes <dener.fernandes.oliveira@gmail.com>
2023-09-13 17:37:25 -0400

    first commit

---
+++ app/src/main/java/com/example/primeirotrabalho/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.primeirotrabalho;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AnimalsFragment animalsFragment;
+    private NumbersFragment numbersFragment;
+    private Spinner menuSpinner;
+
+    String[] menuOptions = {"Home", "Animais", "Números"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Pegando o componente
+
+        menuSpinner = findViewById(R.id.spinnerMenu);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_spinner_dropdown_item, menuOptions);
+        menuSpinner.setAdapter(adapter);
+
+        // Pegando o item selecionado
+
+        menuSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 1) {
+                    animalsFragment = new AnimalsFragment();
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.frameContent, animalsFragment);
+                    transaction.commit();
+                }
+                else if (i == 2) {
+                    numbersFragment = new NumbersFragment();
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.frameContent, numbersFragment);
+                    transaction.commit();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+
+    }
+}
