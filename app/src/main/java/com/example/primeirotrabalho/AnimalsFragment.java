997dce4b42f2c08bdedaff6a31d29701d2744f39
Dener Fernandes <dener.fernandes.oliveira@gmail.com>
2023-09-14 14:36:02 -0400

    Missing home

--- app/src/main/java/com/example/primeirotrabalho/AnimalsFragment.java
+++ app/src/main/java/com/example/primeirotrabalho/AnimalsFragment.java
@@ -1,5 +1,6 @@
 package com.example.primeirotrabalho;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,74 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 public class AnimalsFragment extends Fragment {
+    private ListView animalsNamesList;
+    private String[] itens ={"Gato", "Cachorro", "Leão", "Macaco", "Vaca", "Ovelha"};
+
+    private MediaPlayer mediaplayer;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_animals, container, false);
+        View view = inflater.inflate(R.layout.fragment_animals, container, false);
+
+        animalsNamesList = (ListView) view.findViewById(R.id.animalsNamesList);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, itens);
+
+        // Adicionando adaptador para a lista
+        animalsNamesList.setAdapter(adapter);
+
+        animalsNamesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == 0) {
+                    mediaplayer = MediaPlayer.create(getContext(), R.raw.cat);
+                    execute();
+                }
+                else if( i == 1) {
+                    mediaplayer = MediaPlayer.create(getContext(), R.raw.dog);
+                    execute();
+                }
+                else if( i == 2) {
+                    mediaplayer = MediaPlayer.create(getContext(), R.raw.lion);
+                    execute();
+                }
+                else if( i == 3) {
+                    mediaplayer = MediaPlayer.create(getContext(), R.raw.monkey);
+                    execute();
+                }
+                else if( i == 4) {
+                    mediaplayer = MediaPlayer.create(getContext(), R.raw.cow);
+                    execute();
+                }
+                else if( i == 5) {
+                    mediaplayer = MediaPlayer.create(getContext(), R.raw.sheep);
+                    execute();
+                }
+            }
+        });
+
+        return view;
+    }
+
+    public void execute() {
+        if (mediaplayer != null) {
+            mediaplayer.start();
+            mediaplayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mediaPlayer.release(); // Libera o MediaPlayer quando a reprodução for concluída.
+                }
+            });
+        }
     }
 }
--- app/src/main/java/com/example/primeirotrabalho/NumbersFragment.java
+++ app/src/main/java/com/example/primeirotrabalho/NumbersFragment.java
@@ -1,5 +1,6 @@
 package com.example.primeirotrabalho;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,12 +8,86 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class NumbersFragment extends Fragment {
+    private ImageButton buttonOne, buttonTwo, buttonThree, buttonFour, buttonFive, buttonSix;
+    private MediaPlayer mediaplayer;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numbers, container, false);
+        View view = inflater.inflate(R.layout.fragment_numbers, container, false);
+
+        buttonOne = view.findViewById(R.id.buttonOne);
+        buttonTwo = view.findViewById(R.id.buttonTwo);
+        buttonThree = view.findViewById(R.id.buttonThree);
+        buttonFour = view.findViewById(R.id.buttonFour);
+        buttonFive = view.findViewById(R.id.buttonFive);
+        buttonSix = view.findViewById(R.id.buttonSix);
+
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaplayer = MediaPlayer.create(getContext(), R.raw.one);
+                execute();
+            }
+        });
+
+        buttonTwo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaplayer = MediaPlayer.create(getContext(), R.raw.two);
+                execute();
+            }
+        });
+
+        buttonThree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaplayer = MediaPlayer.create(getContext(), R.raw.three);
+                execute();
+            }
+        });
+
+        buttonFour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaplayer = MediaPlayer.create(getContext(), R.raw.four);
+                execute();
+            }
+        });
+
+        buttonFive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaplayer = MediaPlayer.create(getContext(), R.raw.five);
+                execute();
+            }
+        });
+
+        buttonSix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaplayer = MediaPlayer.create(getContext(), R.raw.six);
+                execute();
+            }
+        });
+
+
+        return view;
+    }
+
+    public void execute() {
+        if (mediaplayer != null) {
+            mediaplayer.start();
+            mediaplayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mediaPlayer.release(); // Libera o MediaPlayer quando a reprodução for concluída.
+                }
+            });
+        }
     }
 }
